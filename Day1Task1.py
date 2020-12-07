a7919d584bd09093d762a83579b599547ee3dfdc
Kirstin Purves <kirstin.purves@kcl.ac.uk>
2020-12-07 15:45:39 +0000

    ran code on real input data

--- Day1Task1.py
+++ Day1Task1.py
@@ -2,15 +2,13 @@
 # Day 1
 # KLPurves
 
-# Example dataframe (from description)
-
-import pandas as pd
-
-dtest = {'Expenses':[1720,979,366,299,1721,1456]}
-
-exp_df = pd.DataFrame(data=dtest)
-
 # task: find the two numbers that add up to 2020. Multiply them.
+import pandas as pd
+import os
+
+# example from site to develop simplest version of function
+dtest = {'Expenses':[1720,979,366,299,1721,1456]}
+exp_df = pd.DataFrame(data=dtest)
 
 """Function that 1. takes the number in the first row and adds it to each qubsequent number one by one 2) checks if the total of this is 2020 (or user specifed amount)
 3) if none work, function takes the number in the second row and repeats. 4)when it finds a combination that equals 2020, it stops and multiplies them, returning the output"""
@@ -29,8 +27,6 @@ def add_compare(x,ylist,match):
         else:
             continue
 
-
-
 # function to start with each index value one by one, comparing to the remainder
 
 def find_expenses(df,amount):
@@ -46,3 +42,9 @@ def find_expenses(df,amount):
 
 # run function, idetnfiy the two numbers and the answer
 find_expenses(exp_df,2020)
+
+# do this with real input.
+workdir = os.getcwd()
+input = pd.read_csv("{0}/day1task1input.csv".format(workdir), header=None)
+
+find_expenses(input,2020)
