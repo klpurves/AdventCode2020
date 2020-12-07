00401f76004f5503f5ab1123772096e6c381a3c4
Kirstin Purves <kirstin.purves@kcl.ac.uk>
2020-12-07 15:49:10 +0000

    tidied code

--- Day1Task1.py
+++ Day1Task1.py
@@ -7,15 +7,20 @@ import pandas as pd
 import os
 
 # example from site to develop simplest version of function
-dtest = {'Expenses':[1720,979,366,299,1721,1456]}
+dtest = {'Expenses': [1720, 979, 366, 299, 1721, 1456]}
 exp_df = pd.DataFrame(data=dtest)
 
-"""Function that 1. takes the number in the first row and adds it to each qubsequent number one by one 2) checks if the total of this is 2020 (or user specifed amount)
-3) if none work, function takes the number in the second row and repeats. 4)when it finds a combination that equals 2020, it stops and multiplies them, returning the output"""
+"""Function that 1. takes the number in the first row and adds it to each
+subsequent number one by one 2) checks if the total of this is 2020 (or user
+specifed amount)3) if none work, function takes the number in the second row
+and repeats. 4)when it finds a combination that equals 2020, it stops and
+multiplies them, returning the output"""
 
 
-# function to compare starting value to a list and print the answer if the correct combination is found
-def add_compare(x,ylist,match):
+# function to compare starting value to a list and print the answer if the
+# correct combination is found
+
+def add_compare(x, ylist, match):
 
     for i in ylist:
         compare = int(i[0])
@@ -27,24 +32,28 @@ def add_compare(x,ylist,match):
         else:
             continue
 
-# function to start with each index value one by one, comparing to the remainder
+# function to start with each index value one by one,
+# comparing to the remainder
 
-def find_expenses(df,amount):
-    n_lines = len(df) # get the number of entries to parse
 
-    #loop through the indexes adding each other one and checking if it adds to user specified amount
+def find_expenses(df, amount):
+    n_lines = len(df)   # get the number of entries to parse
+
+    # loop through the indexes adding each other one and
+    # checking if it adds to user specified amount
     for starting in range(n_lines):
         startval = int(df.iloc[starting])
         other_vals = df.iloc[starting+1:n_lines].values.tolist()
 
-        final = add_compare(startval,other_vals,amount)
+        final = add_compare(startval, other_vals, amount)
     return(final)
 
+
 # run function, idetnfiy the two numbers and the answer
-find_expenses(exp_df,2020)
+find_expenses(exp_df, 2020)
 
 # do this with real input.
 workdir = os.getcwd()
 input = pd.read_csv("{0}/day1task1input.csv".format(workdir), header=None)
 
-find_expenses(input,2020)
+find_expenses(input, 2020)
